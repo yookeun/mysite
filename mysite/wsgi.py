07d80d78d0c60bc7f0af64981ebfb95ede1578e2
yookeun <knoc720@gmail.com>
2021-03-07 18:16:10 +0900

    질문-등록처리 및 부트스트랩 적용

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for mysite project.
+WSGI mysite for mysite project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
--- pybo/views.py
+++ pybo/views.py
@@ -1,7 +1,40 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-# Create your views here.
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Question
+from django.utils import timezone
 
 
 def index(request):
-    return HttpResponse("안녕하세요 pybo에 오신 것을 환영합니다.")
+    """
+    pybo 목록 출력
+    :param request:
+    :return:
+    """
+    question_list = Question.objects.order_by('-create_date')
+    context = {'question_list': question_list}
+    return render(request, 'pybo/question_list.html', context)
+
+
+def detail(request, question_id):
+    """
+    pybo 내용출력
+    :param request:
+    :param question_id:
+    :return:
+    """
+    question = get_object_or_404(Question, pk=question_id)
+    context = {'question': question}
+    return render(request, 'pybo/question_detail.html', context)
+
+
+def answer_create(request, question_id):
+    """
+    pybo 답변등록
+    :param request:
+    :param question_id:
+    :return:
+    """
+    question = get_object_or_404(Question, pk=question_id)
+    question.answer_set.create(content=request.POST.get('content'),
+                               create_date=timezone.now())
+    return redirect('pybo:detail', question_id=question_id)
+
